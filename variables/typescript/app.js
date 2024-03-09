635deeb4aacc73f9de652bf3412a8ceab4e873b2
khazra khan <ayanazam709@gmail.com>
2024-03-10 04:12:36 +0500

    Add files via upload

---
+++ variables/typescript/app.js
@@ -0,0 +1,47 @@
+var firstname = "ali";
+console.log(firstname);
+firstname = "ahmed ali";
+console.log(firstname);
+console.log(firstname);
+// number
+var age = 18;
+console.log(age);
+//boolean
+var scam = false;
+console.log(scam);
+//string
+var message = "khazra";
+console.log(message);
+//const
+var gravity = 9.8;
+console.log(gravity);
+var message = "khazra";
+console.log(message);
+//addition
+var num1 = 2;
+var num2 = 3;
+var cart = num1 + num2;
+console.log(cart);
+//subbraction
+var num3 = 5;
+var num4 = 2;
+console.log(num3 - num4);
+//multipilication 
+console.log(num3 * num4);
+console.log(num3 / num4);
+//exponentiation
+console.log(Math.pow(num3, num4));
+//postfix(addition)
+var a = 4;
+console.log(a++);
+console.log(a++);
+//prefix(addition)
+console.log(++a);
+console.log(++a);
+//prefix(subraction)
+console.log(--a);
+console.log(--a);
+console.log(--a);
+//postfix(subsraction)
+console.log(a--);
+console.log(a--);
